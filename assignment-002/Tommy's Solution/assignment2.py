b1cc2cf857add70441f0c5182f3cb530cf433878
Thomas <tjm165@case.edu>
2019-09-14 10:43:31 -0400

    fiexed bug

--- assignment-002/Tommy's Solution/assignment2.py
+++ assignment-002/Tommy's Solution/assignment2.py
@@ -3,14 +3,18 @@ import sys
 def get_line_width(line):
     keyword = "width"
     line = line[line.index(keyword) + len(keyword):]
-    return int(''.join(filter(str.isdigit, line)))
+    line = line.strip()
+    for i in range(0, len(line)):
+        if not line[i].isdigit():
+            return int(line[:i])
+    return int(line)
 
 def get_total_width_of_schema(schema_file_name):
     schema_file = open(schema_file_name, 'r')
     total_width = 0
     for line in schema_file:
         total_width += get_line_width(line) if line[0] != "#" else 0
-    
+
     schema_file.close()
     return total_width
 
@@ -20,15 +24,14 @@ def read_data(data_file_name, line_width):
     num_lines_without_line_width = 0
 
     for line in data_file:
-        if len(line) == line_width - 1:
+        if len(line) == line_width + 1:
             num_lines_with_line_width += 1
         else:
             num_lines_without_line_width += 1
-        print(len(line))
     return num_lines_with_line_width, num_lines_without_line_width
 
-total_width = get_total_width_of_schema("assignment-002\example-002\schema.txt")
-print(read_data("assignment-002\example-002\data.txt", total_width))
+total_width = get_total_width_of_schema("assignment-002\example-003\schema.txt")
+print(read_data("assignment-002\example-003\data.txt", total_width))
 
 
 
