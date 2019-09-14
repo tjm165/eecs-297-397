5c1ed6c398efc1af20c9c0e305e106fd05495f72
Thomas <tjm165@case.edu>
2019-09-14 02:04:10 -0400

    prints the expected output

--- assignment-002/Tommy's Solution/assignment2.py
+++ assignment-002/Tommy's Solution/assignment2.py
@@ -2,7 +2,6 @@ import sys
 
 def get_line_width(line):
     keyword = "width"
-    line = line.strip()
     line = line[line.index(keyword) + len(keyword):]
     return int(''.join(filter(str.isdigit, line)))
 
@@ -15,10 +14,21 @@ def get_total_width_of_schema(schema_file_name):
     schema_file.close()
     return total_width
 
-data_file_name = "assignment-002\example-002\data.txt"
+def read_data(data_file_name, line_width):
+    data_file = open(data_file_name, 'r')
+    num_lines_with_line_width = 0
+    num_lines_without_line_width = 0
 
-print(get_total_width_of_schema("assignment-002\example-002\schema.txt"))
+    for line in data_file:
+        if len(line) == line_width - 1:
+            num_lines_with_line_width += 1
+        else:
+            num_lines_without_line_width += 1
+        print(len(line))
+    return num_lines_with_line_width, num_lines_without_line_width
 
+total_width = get_total_width_of_schema("assignment-002\example-002\schema.txt")
+print(read_data("assignment-002\example-002\data.txt", total_width))
 
 
 
