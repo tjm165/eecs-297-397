d5df6d22a704d8e5a9a97dfb4a4cd4fda4b82b5b
Thomas <tjm165@case.edu>
2019-10-02 12:36:25 -0400

    tested both functions

--- assignment-003/assignment3.py
+++ assignment-003/assignment3.py
@@ -1,4 +1,5 @@
-# Implement this function:
+# Author tjm165
+
 def store_checkout(inventory_tuple_list, item_purchase_list):
     """
     inventory_tuple_list: 
@@ -8,6 +9,8 @@ def store_checkout(inventory_tuple_list, item_purchase_list):
 
         Example: [("A", 5, "shiny new A"), ("B", 10, "big heavy B")]
     """
+
+    # First, lets put all the inventory items into a name to price dictionary
     inventory_name_to_price = {}
     for item in inventory_tuple_list:
         inventory_name_to_price[item[0]] = item[1]
@@ -26,14 +29,22 @@ def store_checkout(inventory_tuple_list, item_purchase_list):
     The example inputs here would have a total cost of:
     5 + 5 + 10 + 0 = 20 
     """
+    # next let's go through the purchase list. 
+    # We can use the name to price dicionary to quickly get the prices
     total_price = 0
     for item in item_purchase_list:
-        total_price += inventory_name_to_price[item]
+        if item in inventory_name_to_price:
+            total_price += inventory_name_to_price[item]
 
     return total_price
 
+def store_checkout_test():
+    inventory_tuple_list = [("A", 5, "shiny new A"), ("B", 10, "big heavy B"), ("C", 20, "big heavy B")]
+    item_purchase_list = ["A", "A", "B", "C", "D"]
 
-# Implement this function:
+    # expected checkout price is 5 + 5 + 10 + 20 + 0 = 40
+    return 40 == store_checkout(inventory_tuple_list, item_purchase_list)
+
 def highest_frequency_count(item_list):
     """
     item_list:
@@ -50,19 +61,24 @@ def highest_frequency_count(item_list):
     only 1 time. Therefore, expected return value would be 3.
     """
 
+    # let's use an item to frequency dictionary to quickly count all the frequencies
     item_frequency = {}
     for item in item_list:
         if item not in item_frequency:
             item_frequency[item] = 0
         item_frequency[item] = item_frequency[item] + 1
 
-    print("hi" )
-
+    # finally, let's go through the dictionary and find the highest frequency
     highest_frequency = 0
     for item in item_frequency:
-        print("here" )
         if item_frequency[item] > highest_frequency:
             highest_frequency = item_frequency[item]
         
     return highest_frequency
 
+def highest_frequency_count_test():
+    item_list = ["A", "A", "B", "C", "A", "B", "B", "B", "C", "D"]
+    # expected highest frequency is 4
+    return 4 == highest_frequency_count(item_list)
+
+highest_frequency_count_test()
